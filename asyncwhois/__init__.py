2340b1b80a26318964635c7be75ecabffac183c2
pogzyb <pogzyb@umich.edu>
2020-04-01 22:47:37 -0400

    Add lookup entrypoint function

---
+++ asyncwhois/__init__.py
@@ -0,0 +1,69 @@
+import logging
+import asyncio
+import aiodns
+import re
+import os
+
+from .parser import WhoisEntry
+from .query import do_async_whois_query
+
+logger = logging.getLogger("async-whois")
+logger.setLevel(logging.DEBUG)
+
+
+# thanks to https://www.regextester.com/104038
+IPV4_OR_V6 = re.compile(r"((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))")
+
+
+async def lookup(url: str) -> WhoisEntry:
+    domain = await extract_domain(url)
+    query_result = await do_async_whois_query(domain)
+    return WhoisEntry.load(domain, query_result)
+
+
+def cache(func):
+    simple_cache = {}
+
+    def reader(data):
+        if data not in simple_cache:
+            simple_cache[data] = func(data)
+        return simple_cache[data]
+    return reader
+
+
+@cache
+def load_suffixes(tld_file_path: str):
+    with open(tld_file_path, encoding='utf-8') as tlds_fp:
+        suffixes = set(
+            line.encode('utf-8') for line in tlds_fp.read().splitlines() if line and not line.startswith('//')
+        )
+    return suffixes
+
+
+async def extract_domain(url):
+    """Extract the domain from the given URL"""
+    if IPV4_OR_V6.match(url):
+        loop = asyncio.get_event_loop()
+        resolver = aiodns.DNSResolver(loop=loop)
+        # this is an IP address
+        return await resolver.gethostbyaddr(url)
+
+    # downloaded from https://publicsuffix.org/list/public_suffix_list.dat
+    tlds_path = os.path.join(os.getcwd(), os.path.dirname(__file__), 'data', 'public_suffix_list.dat')
+    suffixes = load_suffixes(tlds_path)
+
+    if not isinstance(url, str):
+        url = url.decode('utf-8')
+    url = re.sub('^.*://', '', url)
+    url = url.split('/')[0].lower()
+
+    # find the longest suffix match
+    domain = b''
+    for section in reversed(url.split('.')):
+        if domain:
+            domain = b'.' + domain
+        domain = section.encode('utf-8') + domain
+        if domain not in suffixes:
+            break
+    return domain.decode('utf-8')
+
