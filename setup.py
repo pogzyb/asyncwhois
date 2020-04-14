781998d0e561ad2fd97ab373da01efdcd29beb83
Joe <pogzyb@umich.edu>
2020-04-14 09:10:44 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asyncwhois",
+    version="0.1.0",
+    author="Joseph Obarzanek",
+    author_email="pogzyb@umich.edu",
+    description="Async-compatible Python module for retrieving WHOIS information for domains.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'aiodns',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+        'Framework :: AsyncIO'
+    ],
+    url="https://github.com/pogzyb/asyncwhois",
+    packages='asyncwhois',
+    packages_dir={'asyncwhois': 'asyncwhois'},
+    python_requires='>=3.6',
+)
