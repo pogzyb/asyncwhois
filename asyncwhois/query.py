d88a23a1294863fcb4d2d40f8891cc7cf9164f72
Joe <pogzyb@umich.edu>
2020-04-13 21:23:08 -0400

    Clean up comments in query.py

--- asyncwhois/query.py
+++ asyncwhois/query.py
@@ -5,27 +5,20 @@ import asyncio
 logger = logging.getLogger(__name__)
 
 
-class WhoIsCommandFailure(Exception):
-    pass
-
-
-async def do_async_whois_query(domain: str, ignore_returncode: bool=True):
+async def do_async_whois_query(domain: str) -> str:
     """
-    Runs the "whois <domain>" command in an asyncio subprocess shell.
-    Captures stdout and stderr. Checks stderr if ignore_returncode is False.
+    Runs the "whois <domain>" command asynchronously in a different "Process".
+    (stderr is captured but not used at this time; could also be parsed in the future).
 
-    :param domain:
-    :param ignore_returncode:
-    :return: whois output string
+    :param domain: The domain for the command: "whois <domain>"
+    :return: Raw WhoIs output string
     """
-    logger.debug(f'Running "whois" query for _{domain}_')
+    logger.debug(f'Running "whois" query for {domain}')
     # https://docs.python.org/3/library/asyncio-subprocess.html
     p = await asyncio.create_subprocess_shell(
         f"whois {domain}",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
-    query_result, stderr = await p.communicate()
-    if not ignore_returncode and p.returncode != 0:
-        raise WhoIsCommandFailure(query_result)
+    query_result, _ = await p.communicate()
     return query_result.decode()
