a2f47982305270ce52391fae7656c90463f0af54
Joe <pogzyb@umich.edu>
2020-04-26 19:42:38 -0400

    Update tests with assertions

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -8,32 +8,32 @@ from asyncwhois import WhoisEntry
 class TestParseWhoIsText(asynctest.TestCase):
 
     def setUp(self):
-        self.base_path = '/Users/josepho/Desktop/py-practice/async-pywhois/tests/samples/'
+        self.base_path = os.path.join(os.getcwd(), 'samples')
 
     def test_parse_top(self):
         domain = 'com-wu.top'
-        with open(os.path.join(self.base_path, 'tld_top.txt'), 'r') as topfile: 
-            text = topfile.read()
+        with open(os.path.join(self.base_path, 'tld_top.txt'), 'r') as tldfile:
+            text = tldfile.read()
         top = WhoisEntry.load(domain, text)
-        print(top)
+        self.assertIsNotNone(top.get('creation_date'))
 
-    # def test_parse_ir(self):
-    #     domain = 'masoudrahimi.ir'
-    #     with open(os.path.join(self.base_path, 'tld_ir.txt'), 'r') as topfile: 
-    #         text = topfile.read()
-    #     ir = WhoisEntry.load(domain, text)
-    #     print(ir)
+    def test_parse_ir(self):
+        domain = 'masoudrahimi.ir'
+        with open(os.path.join(self.base_path, 'tld_ir.txt'), 'r') as tldfile:
+            text = tldfile.read()
+        ir = WhoisEntry.load(domain, text)
+        self.assertIsNotNone(ir.get('updated_date'))
 
-    # def test_parse_xyz(self):
-    #     domain = 'redemtoin.xyz'
-    #     with open(os.path.join(self.base_path, 'tld_xyz.txt'), 'r') as topfile: 
-    #         text = topfile.read()
-    #     xyz = WhoisEntry.load(domain, text)
-    #     print(xyz)
+    def test_parse_xyz(self):
+        domain = 'redemtoin.xyz'
+        with open(os.path.join(self.base_path, 'tld_xyz.txt'), 'r') as tldfile:
+            text = tldfile.read()
+        xyz = WhoisEntry.load(domain, text)
+        self.assertIsNotNone(xyz.get('creation_date'))
 
-    # def test_parse_icu(self):
-    #     domain = 'skyline-empire.icu'
-    #     with open(os.path.join(self.base_path, 'tld_icu.txt'), 'r') as topfile: 
-    #         text = topfile.read()
-    #     icu = WhoisEntry.load(domain, text)
-    #     print(icu)
+    def test_parse_icu(self):
+        domain = 'skyline-empire.icu'
+        with open(os.path.join(self.base_path, 'tld_icu.txt'), 'r') as tldfile:
+            text = tldfile.read()
+        icu = WhoisEntry.load(domain, text)
+        self.assertIsNotNone(icu.get('creation_date'))
