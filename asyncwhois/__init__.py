df26fb8b8428fcd081ed4bbf0c1d5fe885ebb3ef
Joe <pogzyb@umich.edu>
2020-06-08 14:59:59 -0400

    Add timeout parameter to entrypoints

--- asyncwhois/__init__.py
+++ asyncwhois/__init__.py
@@ -5,25 +5,27 @@ __all__ = ['lookup', 'aio_lookup', 'has_parser_support']
 __version__ = '0.2.0'
 
 
-def lookup(url: str) -> _PyWhoIs:
+def lookup(url: str, timeout: int = 10) -> _PyWhoIs:
     """
     Module entry point for whois lookups.
 
     :param url: Any correctly formatted URL (e.g. https://en.wikipedia.org/wiki/WHOIS)
+    :param timeout: whois server connection timeout (default 10 seconds)
     :return: instance of _PyWhoIs with "query_output" and "parser_output" attributes
     """
-    whois = _PyWhoIs._from_url(url)
+    whois = _PyWhoIs._from_url(url, timeout)
     return whois
 
 
-async def aio_lookup(url: str) -> _PyWhoIs:
+async def aio_lookup(url: str, timeout: int = 10) -> _PyWhoIs:
     """
     Async module entry point for whois lookups.
 
-   :param url: Any correctly formatted URL (e.g. https://en.wikipedia.org/wiki/WHOIS)
+    :param url: Any correctly formatted URL (e.g. https://en.wikipedia.org/wiki/WHOIS)
+    :param timeout: whois server connection timeout (default 10 seconds)
     :return: instance of _PyWhoIs with "query_output" and "parser_output" attributes
     """
-    whois = await _PyWhoIs._aio_from_url(url)
+    whois = await _PyWhoIs._aio_from_url(url, timeout)
     return whois
 
 
