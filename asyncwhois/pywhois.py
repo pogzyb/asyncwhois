0fde29092eab12db2a306b06404a413cc2e86ddd
Joe Obarzanek <pogzyb@umich.edu>
2021-06-12 18:35:30 -0400

    Remove LICENSE text from pywhois.py

--- asyncwhois/pywhois.py
+++ asyncwhois/pywhois.py
@@ -1,27 +1,3 @@
-"""
-WHOIS module written in Python.
-
-Copyright (c) 2020 Joe Obarzanek
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
 import asyncio
 import re
 import socket
@@ -29,8 +5,8 @@ import subprocess
 import sys
 from typing import Union, Dict, Any
 
-import aiodns  # >= 2.0.0
-import tldextract  # >= 2.2.2
+import aiodns
+import tldextract
 
 from .errors import QueryError
 from .parser import WhoIsParser
