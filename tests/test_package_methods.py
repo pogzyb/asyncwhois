7c6d73485af5526e4c17af11c60b4aff7994252c
pogzyb <pogzyb@umich.edu>
2022-02-09 20:06:34 -0500

    Update test names/syntax
    Do to name changes, function calls were tweaked.

--- tests/test_package_methods.py
+++ tests/test_package_methods.py
@@ -5,48 +5,27 @@ import asynctest.mock as aio_mock
 import asyncwhois
 
 
-class TestAsyncWhoIsQuery(asynctest.TestCase):
+class TestExportedFunctions(asynctest.TestCase):
 
-    @aio_mock.patch('asyncwhois.pywhois.PyWhoIs._aio_from_url')
+    @aio_mock.patch('asyncwhois.pywhois.DomainLookup.aio_whois_domain')
     async def test_aio_lookup(self, mock_whois_call):
         mock_query_data = {'parser_output': {'domain_name': 'amazon.com'}, 'query_output': 'Domain Name: amazon.com'}
         mock_whois_call.return_value = mock.Mock(query_output=mock_query_data.get('query_output'),
                                                  parser_output=mock_query_data.get('parser_output'))
         test_domain = "amazon.com"
-        w = await asyncwhois.aio_lookup(test_domain)
+        w = await asyncwhois.aio_whois_domain(test_domain)
         mock_whois_call.assert_called_once()
         self.assertIn(f"domain name: {test_domain}", w.query_output.lower())
         self.assertEqual(w.parser_output.get('domain_name').lower(), test_domain)
 
-    @mock.patch('asyncwhois.pywhois.PyWhoIs._from_url')
+    @mock.patch('asyncwhois.pywhois.DomainLookup.whois_domain')
     def test_lookup(self, mock_whois_call):
         mock_query_data = {'parser_output': {'domain_name': 'elastic.co'}, 'query_output': 'Domain Name: elastic.co'}
         mock_whois_call.return_value = mock.Mock(query_output=mock_query_data.get('query_output'),
                                                  parser_output=mock_query_data.get('parser_output'))
         test_domain = "elastic.co"
-        w = asyncwhois.lookup(test_domain)
+        w = asyncwhois.whois_domain(test_domain)
         mock_whois_call.assert_called_once()
         self.assertIn(f"domain name: {test_domain}", w.query_output.lower())
         self.assertEqual(w.parser_output.get('domain_name').lower(), test_domain)
 
-    @aio_mock.patch('asyncwhois.pywhois.PyWhoIs._aio_from_whois_cmd')
-    async def test_aio_whois_cmd_shell(self, mock_whois_call):
-        mock_query_data = {'parser_output': {'domain_name': 'yahoo.com'}, 'query_output': 'Domain Name: yahoo.com'}
-        mock_whois_call.return_value = mock.Mock(query_output=mock_query_data.get('query_output'),
-                                                 parser_output=mock_query_data.get('parser_output'))
-        test_domain = "yahoo.com"
-        w = await asyncwhois.aio_whois_cmd_shell(test_domain)
-        mock_whois_call.assert_called_once()
-        self.assertIn(f"domain name: {test_domain}", w.query_output.lower())
-        self.assertEqual(w.parser_output.get('domain_name').lower(), test_domain)
-
-    @mock.patch('asyncwhois.pywhois.PyWhoIs._from_whois_cmd')
-    def test_whois_cmd_shell(self, mock_whois_call):
-        mock_query_data = {'parser_output': {'domain_name': 'comcast.net'}, 'query_output': 'Domain Name: comcast.net'}
-        mock_whois_call.return_value = mock.Mock(query_output=mock_query_data.get('query_output'),
-                                                 parser_output=mock_query_data.get('parser_output'))
-        test_domain = "comcast.net"
-        w = asyncwhois.whois_cmd_shell(test_domain)
-        mock_whois_call.assert_called_once()
-        self.assertIn(f"domain name: {test_domain}", w.query_output.lower())
-        self.assertEqual(w.parser_output.get('domain_name').lower(), test_domain)
