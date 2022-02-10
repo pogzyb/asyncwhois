796dfea03fee34461c33419001b2fcef43e86b8d
pogzyb <pogzyb@umich.edu>
2022-02-09 19:34:33 -0500

    Refactor Query Base Class
    The socket related functions were moved into the Query base class. These will be used by DomainQuery and NumberQuery subclasses. Socks proxy support was added to the connection related functions and also converted to context managers. Socket-related errors are no longer handled by asyncwhois.

--- asyncwhois/query.py
+++ asyncwhois/query.py
@@ -1,71 +1,100 @@
 import asyncio
 import re
 import socket
-from typing import Tuple
+import sys
+from typing import Tuple, Generator
+from contextlib import contextmanager
+# different installs for async contextmanager based on python version
+if sys.version_info < (3, 7):
+    from async_generator import asynccontextmanager
+else:
+    from contextlib import asynccontextmanager
 
-from .errors import QueryError
+from python_socks.sync import Proxy
+from python_socks.async_.asyncio import Proxy as AsyncProxy
 
 BLOCKSIZE = 1024
 
 
 class Query:
-    _iana_server = "whois.iana.org"
-    _whois_port = 43
+    iana_server = "whois.iana.org"
+    whois_port = 43
+    refer_regex = r"refer: *(.+)"
+    whois_server_regex = r".+ whois server: *(.+)"
+
+    def __init__(
+        self,
+        server: str = None,
+        authoritative_only: bool = True,
+        proxy_url: str = None,
+        timeout: int = 10
+    ):
+        self.server = server
+        self.authoritative_only = authoritative_only
+        self.proxy_url = proxy_url
+        self.timeout = timeout
+        self.authoritative_server = ""
+        self.query_output = ""
+        self.query_chain = ""
 
     @staticmethod
     def _find_match(regex: str, blob: str) -> str:
         match = ""
         found = re.search(regex, blob, flags=re.IGNORECASE)
         if found:
-            match = found.group(1).rstrip('\r')
+            match = found.group(1).rstrip("\r").replace(" ", "").rstrip(":")
         return match
 
-
-class WhoIsQuery(Query):
-
-    def __init__(self, domain: str, server: str = None, timeout: int = 10):
-        self.domain = domain
-        self.server = server
-        self.timeout = timeout
-        self.query_output = ""
-        self._run()
-
-    def _run(self):
-        """
-        Connects to the given WhoIs server on port 43, submits a query for the given domain,
-        and saves the output into the "query_output" attribute. If server is not specified (defaults to None),
-        then a connection to the IANA root is created first.
-        """
-        data = self.domain + "\r\n"
+    @contextmanager
+    def _create_connection(
+        self,
+        address: Tuple[str, int],
+        proxy_url: str = None
+    ) -> Generator[socket.socket, None, None]:
+        s = None
         try:
-            # server is "None"
-            if not self.server:
-                # reach out to whois.iana.org:43 to find "refer"
-                with self._create_connection((self._iana_server, self._whois_port), self.timeout) as conn:
-                    iana_result_blob = self._send_and_recv(conn, data)
-                    self.server = self._find_match(regex=r"refer: *(.+)", blob=iana_result_blob)
-                    if not self.server:
-                        raise QueryError(f"Could not find a whois server for {self.domain}")
-            # connect to <server>:43
-            with self._create_connection((self.server, self._whois_port), self.timeout) as conn:
-                # save output into "query_output"
-                self.query_output = self._send_and_recv(conn, data)
-                # check for "authoritative" whois server via regex
-                whois_server = self._find_match(regex=r"whois server: *(.+)", blob=self.query_output)
-                whois_server = whois_server.replace(' ', '').rstrip(':')
-                if whois_server and self.server != whois_server:
-                    # if there is a more authoritative source; connect and re-query
-                    self.server = whois_server
-                    with self._create_connection((self.server, self._whois_port), self.timeout) as conn:
-                        # save output into "query_output"
-                        self.query_output = self._send_and_recv(conn, data)
-        
-        except ConnectionRefusedError:
-            raise QueryError(f'')
+            # Use proxy if specified
+            if proxy_url:
+                proxy = Proxy.from_url(proxy_url)
+                # proxy is a standard python socket in blocking mode
+                s = proxy.connect(*address)
+            else:
+                # otherwise use socket
+                s = socket.create_connection(address, self.timeout)
+            yield s
+        finally:
+            if s and hasattr(s, "close"):
+                s.close()
 
-        except ConnectionResetError:
-            server = self.server or self._iana_server
-            raise QueryError(f'"Connection reset by peer" when communicating with {server}:43')
+    @asynccontextmanager
+    async def _aio_create_connection(
+        self,
+        address: Tuple[str, int],
+        proxy_url: str = None
+    ) -> Generator[Tuple[asyncio.StreamReader, asyncio.StreamWriter], None, None]:
+        # init
+        reader, writer = None, None
+        # Use proxy if specified
+        if proxy_url:
+            proxy = AsyncProxy.from_url(proxy_url)
+            # sock is a standard python socket in blocking mode
+            sock = await proxy.connect(*address)
+            # pass it to asyncio
+            s = asyncio.open_connection(
+                host=None,
+                port=None,
+                sock=sock)
+        else:
+            # otherwise use asyncio to open the connection
+            s = asyncio.open_connection(*address)
+        try:
+            reader, writer = await asyncio.wait_for(s, self.timeout)
+            yield reader, writer
+        finally:
+            if writer:
+                writer.close()
+                if hasattr(writer, "wait_closed"):
+                    await writer.wait_closed()
 
     @staticmethod
     def _send_and_recv(conn: socket.socket, data: str) -> str:
@@ -76,68 +105,15 @@ class WhoIsQuery(Query):
             if received == b"":
                 break
             else:
-                result += received.decode('utf-8', errors='ignore')
+                result += received.decode("utf-8", errors="ignore")
         return result
 
     @staticmethod
-    def _create_connection(address: Tuple[str, int], timeout: int) -> socket.socket:
-        try:
-            return socket.create_connection(address=address, timeout=timeout)
-        except socket.timeout:
-            raise QueryError(f'Connection timeout for WHOIS server at {address[0]}:{address[1]}')
-        except socket.gaierror:
-            raise QueryError(f'Could not get address information for {address[0]}:{address[1]}')
-
-
-class AsyncWhoIsQuery(Query):
-
-    def __init__(self, domain: str, server: str, timeout: int):
-        self.domain = domain
-        self.server = server
-        self.timeout = timeout
-        self.query_output = ""
-
-    @classmethod
-    async def create(cls, domain: str, server: str = None, timeout: int = 10):
-        query = cls(domain, server, timeout)
-        await query._run()
-        return query
-
-    async def _run(self) -> None:
-        """
-        Connects to the given WhoIs server on port 43, submits a query for the given domain,
-        and saves the output into the "query_output" attribute. If server is not specified (defaults to None),
-        then a connection to the IANA root is created first.
-        """
-        data = self.domain + "\r\n"
-        try:
-            if not self.server:
-                reader, writer = await self._create_connection((self._iana_server, self._whois_port), self.timeout)
-                iana_query_output = await self._send_and_recv(reader, writer, data)
-                self.server = self._find_match(regex=r"refer: *(.+)", blob=iana_query_output)
-                writer.close()
-                await writer.wait_closed()
-                if not self.server:
-                    raise QueryError(f'Could not find a WHOIS server for {self.domain}')
-
-            reader, writer = await self._create_connection((self.server, self._whois_port), self.timeout)
-            self.query_output = await self._send_and_recv(reader, writer, data)
-            whois_server = self._find_match(regex=r"whois server: *(.+)", blob=self.query_output)
-            whois_server = whois_server.replace(' ', '').rstrip(':')
-            if whois_server:
-                self.server = whois_server
-                reader, writer = await self._create_connection((self.server, self._whois_port), self.timeout)
-                self.query_output = await self._send_and_recv(reader, writer, data)
-
-            writer.close()
-            if hasattr(writer, 'wait_closed'):
-                await writer.wait_closed()
-        except ConnectionResetError:
-            server = self.server or self._iana_server
-            raise QueryError(f'"Connection reset by peer" when communicating with {server}:43')
-
-    @staticmethod
-    async def _send_and_recv(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, data: str) -> str:
+    async def _aio_send_and_recv(
+        reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter,
+        data: str
+    ) -> str:
         writer.write(data.encode())
         result = ""
         while True:
@@ -145,7 +121,7 @@ class AsyncWhoIsQuery(Query):
             if received == b"":
                 break
             else:
-                result += received.decode('utf-8', errors='ignore')
+                result += received.decode("utf-8", errors="ignore")
         return result
 
     @staticmethod
