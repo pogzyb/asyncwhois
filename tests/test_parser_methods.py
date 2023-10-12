7b3260d2c89eab557f5317d316c45412316ea098
Joe Obarzanek <pogzyb@umich.edu>
2023-10-12 13:01:14 -0400

    Add test for mismatched keys

--- tests/test_parser_methods.py
+++ tests/test_parser_methods.py
@@ -1,23 +1,53 @@
 import unittest
 import datetime
+import json
+import os
 
-from asyncwhois.parse import BaseParser
+from asyncwhois.parse import BaseParser, convert_whodap_keys
 
 
 class TestWhoIsParserMethods(unittest.TestCase):
+    def test_convert_whodap_keys(self):
+        with open(
+            os.path.join(
+                os.path.abspath(os.path.dirname(__file__)),
+                "samples/com_dict_asyncwhois.json",
+            )
+        ) as o:
+            asyncwhois_dict = json.loads(o.read())
+
+        with open(
+            os.path.join(
+                os.path.abspath(os.path.dirname(__file__)),
+                "samples/com_dict_whodap.json",
+            )
+        ) as o:
+            whodap_dict = json.loads(o.read())
+
+        whodap_keys_after = convert_whodap_keys(whodap_dict).keys()
+        asyncwhois_keys = asyncwhois_dict.keys()
+        for whodap_key in whodap_keys_after:
+            assert (
+                whodap_key in asyncwhois_keys
+            ), f"{whodap_key} not in {asyncwhois_keys}"
+
+        for asyncwhois_key in asyncwhois_dict.keys():
+            assert (
+                asyncwhois_key in whodap_keys_after
+            ), f"{asyncwhois_key} not in {whodap_keys_after}"
 
     def test_parse_dates(self):
         date_strings = [
-            '11-aug-2020',
-            '11-August-2020',
-            '11-09-2020',
-            '2020-09-20',
-            '2020.09.20',
-            '2020/09/20',
-            '2020. 09. 20.',
-            '2020.09.20 11:11:11',
-            'August 11 2020',
-            '20200920'
+            "11-aug-2020",
+            "11-August-2020",
+            "11-09-2020",
+            "2020-09-20",
+            "2020.09.20",
+            "2020/09/20",
+            "2020. 09. 20.",
+            "2020.09.20 11:11:11",
+            "August 11 2020",
+            "20200920",
         ]
 
         for date_string in date_strings:
@@ -31,11 +61,13 @@ class TestWhoIsParserMethods(unittest.TestCase):
         Name server: ns2.google.com
         Status: ok
         """
-        domain = BaseParser().find_match(r'domain name: *(.+)', test_blob)
+        domain = BaseParser().find_match(r"domain name: *(.+)", test_blob)
         self.assertEqual(domain, "google.com")
-        name_servers = BaseParser().find_match(r'name server: *(.+)', test_blob, many=True)
+        name_servers = BaseParser().find_match(
+            r"name server: *(.+)", test_blob, many=True
+        )
         self.assertEqual(len(name_servers), 2)
-        status = BaseParser().find_match(r'status: *(.+)', test_blob, many=True)
+        status = BaseParser().find_match(r"status: *(.+)", test_blob, many=True)
         self.assertEqual(len(status), 1)
 
     def test_find_multiline_match(self):
@@ -50,5 +82,7 @@ class TestWhoIsParserMethods(unittest.TestCase):
 
         Registrar: someone
         """
-        name_servers = BaseParser().find_multiline_match(r'Domain nameservers:\n', test_blob)
+        name_servers = BaseParser().find_multiline_match(
+            r"Domain nameservers:\n", test_blob
+        )
         self.assertEqual(len(name_servers), 4)
