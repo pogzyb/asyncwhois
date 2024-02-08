61dcd34e426ce2f5e9593940f76c4d997230a785
Joe Obarzanek <pogzyb@umich.edu>
2024-02-08 18:59:37 -0500

    Update setup configs

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setuptools.setup(
     license="MIT",
     install_requires=[
         "python-socks[asyncio]>=2.0.2",
-        "tldextract>=2.2.0",
-        "whodap>=0.1.4",
+        "tldextract>=3.2.0",
+        "whodap>=0.1.10",
     ],
     classifiers=[
         "Environment :: Web Environment",
@@ -36,11 +36,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development",
@@ -50,5 +51,5 @@ setuptools.setup(
     url="https://github.com/pogzyb/asyncwhois",
     packages=["asyncwhois"],
     package_dir={"asyncwhois": "asyncwhois"},
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
