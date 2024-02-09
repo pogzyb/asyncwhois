f6a7748611d1d8c471b83c65a9afbd2769cdaf02
Joe Obarzanek <pogzyb@umich.edu>
2024-02-08 19:14:20 -0500

    Remove py37 and py38 support

--- setup.py
+++ setup.py
@@ -36,8 +36,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -51,5 +49,5 @@ setuptools.setup(
     url="https://github.com/pogzyb/asyncwhois",
     packages=["asyncwhois"],
     package_dir={"asyncwhois": "asyncwhois"},
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
