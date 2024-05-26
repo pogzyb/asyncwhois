1183371d5d2a5e6aa7bb9807f0c90a7cf0cd20d8
Joe Obarzanek <pogzyb@umich.edu>
2024-05-25 22:00:15 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
         "Framework :: AsyncIO",
     ],
     url="https://github.com/pogzyb/asyncwhois",
-    packages=["asyncwhois", "asyncwhois.servers"],
+    packages=setuptools.find_packages(),
     package_dir={"asyncwhois": "asyncwhois"},
     python_requires=">=3.9",
 )
